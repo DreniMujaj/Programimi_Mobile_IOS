9eef2f0056ec25ab7c0f5b9b011c8659398f6b69
mujajdreni@gmail.com <mujajdreni@gmail.com>
2023-09-20 20:34:04 +0200

    Initial Commit

---
+++ programimimobileios2023/programimimobileios2023App.swift
@@ -0,0 +1,17 @@
+//
+//  programimimobileios2023App.swift
+//  programimimobileios2023
+//
+//  Created by Dreni Mujaj on 20.9.23.
+//
+
+import SwiftUI
+
+@main
+struct programimimobileios2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
