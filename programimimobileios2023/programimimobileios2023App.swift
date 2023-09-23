ebf17249150f3b1f70fee67f5596a2bd348fa4e6
blendipovataj <blendpovataj@gmail.com>
2023-09-23 23:16:37 +0200

    Blendi povataj: create new UI with user input and add navigation to main page. Make a request to backend to save the player name.

--- programimimobileios2023/programimimobileios2023App.swift
+++ programimimobileios2023/programimimobileios2023App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct programimimobileios2023App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PlayerInputView()
         }
     }
 }
